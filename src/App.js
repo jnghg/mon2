c2ee2150506721aa3cb1609543ba3560f18c95d8
hg <eniows@naver.com>
2021-02-10 23:58:44 +0900

    samsung, lof 마무리

--- src/App.js
+++ src/App.js
@@ -93,9 +93,12 @@ class App extends Component {
       $("body").removeClass("nature-on");
     });
 
-    // custom
+    ////// 삼성 작업 ////////
+
+    // 프로모션 클릭
     $("#promotion").on("click", function () {
       $(".dmBanner").hide();
+      $(".poster").hide();
       $(".promotion").show();
 
       // 클릭버튼 표시
@@ -105,10 +108,15 @@ class App extends Component {
       // 다른버튼 css지우기
       $("#dmBanner").css('font-weight', '');
       $("#dmBanner").css('font-size', '');
+      $("#poster").css('font-weight', '');
+      $("#poster").css('font-size', '');
 
     });
+
+    // DM배너 클릭
     $("#dmBanner").on("click", function () {
       $(".promotion").hide();
+      $(".poster").hide();
       $(".dmBanner").show();
 
       // 클릭버튼 표시
@@ -118,14 +126,69 @@ class App extends Component {
       // 다른버튼 css지우기
       $("#promotion").css('font-weight', '');
       $("#promotion").css('font-size', '');
+      $("#poster").css('font-weight', '');
+      $("#poster").css('font-size', '');
+    });
+
+    // poster클릭
+    $("#poster").on("click", function () {
+      $(".promotion").hide();
+      $(".dmBanner").hide();
+      $(".poster").show();
+
+      // 클릭버튼 표시
+      $("#poster").css('font-weight', 'bold');
+      $("#poster").css('font-size', '17px');
+
+      // 다른버튼 css지우기
+      $("#promotion").css('font-weight', '');
+      $("#promotion").css('font-size', '');
+      $("#dmBanner").css('font-weight', '');
+      $("#dmBanner").css('font-size', '');
+    });
+
+    //////  Lof 작업 //////
+
+    // harness 클릭
+    $("#harness").on("click", function () {
+      $(".collar").hide();
+      $(".harness").show();
+
+      // 클릭버튼 표시
+      $("#harness").css('font-weight', 'bold');
+      $("#harness").css('font-size', '17px');
+
+      // 다른버튼 css지우기
+      $("#collar").css('font-weight', '');
+      $("#collar").css('font-size', '');
+    });
+
+    // collar 클릭
+    $("#collar").on("click", function () {
+      $(".collar").show();
+      $(".harness").hide();
+
+      // 클릭버튼 표시
+      $("#collar").css('font-weight', 'bold');
+      $("#collar").css('font-size', '17px');
+
+      // 다른버튼 css지우기
+      $("#harness").css('font-weight', '');
+      $("#harness").css('font-size', '');
     });
 
     // 초기로딩 클릭버튼 표시
     $("#promotion").css('font-weight', 'bold');
     $("#promotion").css('font-size', '17px');
-    
+
+    $("#harness").css('font-weight', 'bold');
+    $("#harness").css('font-size', '17px');
+
     // 초기 배너 숨기기
-    $(".dmBanner").hide();
+    $(".dmBanner").hide();    //  dm
+    $(".poster").hide();      // poster
+    $(".collar").hide();      // poster
+
   }
 
   render() {
@@ -170,10 +233,6 @@ class App extends Component {
                     92.10.10
                   </p>
                   <p className="profile_word">
-                    <b>Phone</b> <br />
-                    010-7548-4495
-                  </p>
-                  <p className="profile_word">
                     <b>Experience</b> <br />
                     2018 ~ 2020 <br />
                     삼성전자 패밀리넷 디자이너
@@ -197,13 +256,16 @@ class App extends Component {
             <div className="container">
               <div className="row justify-content-center">
                 <div className="col-12 text-center">
-                  <a href="#" className="hover-target">ramirumi@naver.com</a>
+                  <a href="#" className="hover-target">ramirumi@naver.com</a><br/>
+                  <a href="#" className="hover-target">010-7548-4495</a>
                 </div>
 
               </div>
             </div>
           </div>
         </div>
+
+        {/* 삼성전자 임직원몰 */}
 
         <div className="travel-section">
           <div className="travel-close hover-target"></div>
@@ -217,7 +279,7 @@ class App extends Component {
               </div>
               <div className="col-12 text-center">
                 <p className="familynet">
-                  <font className = "travel hover-target" id="promotion">Promotion</font> / <font className = "travel hover-target" id="dmBanner">Direct Mailing</font>
+                  <font className = "travel hover-target" id="promotion">Promotion</font> / <font className = "travel hover-target" id="dmBanner">Direct Mailing</font> / <font className = "travel hover-target" id="poster">Poster</font>
                 </p>
               </div>
 
@@ -226,10 +288,17 @@ class App extends Component {
               <br/>
 
               {/* 프로모션 배너 */}
-
-                {/* pcMobile */}
                 <div className="col-md-6 col-lg-10 promotion">
                   <img src="/images/banner/pcMobile/phone_gal.jpg" alt="" />
+                </div>
+                <div className="col-md-6 col-lg-10 promotion">
+                  <img src="/images/banner/dress/1912_event_clothes_2.jpg" alt="" />
+                </div>
+                <div className="col-md-6 col-lg-10 promotion">
+                  <img src="/images/promotion/muhan.jpg" alt="" />
+                </div>
+                <div className="col-md-6 col-lg-10 promotion">
+                  <img src="/images/banner/life/2001_event_ac_1.jpg" alt="" />
                 </div>
                 <div className="col-md-6 col-lg-10 promotion">
                   <img src="/images/promotion/1908_event_wedding.jpg" alt="" />
@@ -238,18 +307,10 @@ class App extends Component {
                   <img src="/images/promotion/2003_event_energy_2.jpg" alt="" />
                 </div>
                 <div className="col-md-6 col-lg-10 promotion">
-                  <img src="/images/banner/pcMobile/notebook_banner.jpg" alt="" />
+                  <img src="/images/banner/life/2003_event_air_filter.jpg" alt="" />
                 </div>
                 <div className="col-md-6 col-lg-10 promotion">
                   <img src="/images/banner/pcMobile/2009_event_monitor.jpg" alt="" />
-                </div>
-
-                {/* life */}
-                <div className="col-md-6 col-lg-10 promotion">
-                  <img src="/images/promotion/1911_muhan.jpg" alt="" />
-                </div>
-                <div className="col-md-6 col-lg-10 promotion">
-                  <img src="/images/banner/life/2001_event_ac_1.jpg" alt="" />
                 </div>
                 <div className="col-md-6 col-lg-10 promotion">
                   <img src="/images/banner/life/1904_event_air_1.jpg" alt="" />
@@ -258,18 +319,11 @@ class App extends Component {
                   <img src="/images/banner/life/1902_event_ac_1.jpg" alt="" />
                 </div>
                 <div className="col-md-6 col-lg-10 promotion">
-                  <img src="/images/banner/life/2003_event_air_filter.jpg" alt="" />
-                </div>
-
-                {/* dress */}
-                <div className="col-md-6 col-lg-10 promotion">
-                  <img src="/images/banner/dress/1912_event_clothes_2.jpg" alt="" />
+                  <img src="/images/banner/pcMobile/notebook_banner.jpg" alt="" />
                 </div>
                 <div className="col-md-6 col-lg-10 promotion">
                   <img src="/images/banner/dress/1902_event_sew_2.jpg" alt="" />
                 </div>
-
-                {/* kitchen */}
                 <div className="col-md-6 col-lg-10 promotion">
                   <img src="/images/banner/kitchen/re_01.jpg" alt="" />
                 </div>
@@ -288,64 +342,62 @@ class App extends Component {
                   <img src="/images/dm/mail_20190718_dv.jpg" alt="" />
                 </div>
 
+              {/*  Poster  */}
+              <div className="col-md-6 col-lg-12 poster">
+                <img src="/images/poster/poster4.png" alt="" />
+              </div>
+              <div className="col-md-6 col-lg-12 poster">
+                <img src="/images/poster/poster3.png" alt="" />
+              </div>
+
             </div>
           </div>
         </div>
+
+
+      {/* Lof 작업 */}
 
         <div className="wildlife-section">
           <div className="wildlife-close hover-target"></div>
           <div className="container">
             <div className="row justify-content-center">
               <div className="col-12 text-center">
-                <h3>wildlife</h3>
+                <h3>Lof Project</h3>
               </div>
               <div className="col-12 mt-3 text-center">
-                <p><span>Canon PowerShot S95</span></p>
+                <p><span>반려동물 쇼핑몰</span></p>
               </div>
+              
               <div className="col-12 text-center">
-                <p>
-                  focal length: 22.5mm<br />
-                  aperture: ƒ/5.6<br />
-                  exposure time: 1/1000<br />
-                  ISO: 80
+                <p className="familynet">
+                  <font className = "wildlife hover-target" id="harness">Harness</font> / <font className = "wildlife hover-target" id="collar">Collar</font>
                 </p>
               </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
+
+              {/* 하네스 */}
+              <div className="col-md-6 col-lg-4 harness">
+                <img src="/images/lof/lof_h.jpg" alt="" />
               </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
+              <div className="col-md-6 col-lg-4 harness">
+                <img src="/images/lof/lof_amazon3.jpg" alt="" />
               </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
+              <div className="col-md-6 col-lg-4 harness">
+                <img src="/images/lof/thumbnail_1.jpg" alt="" />
+                <img src="/images/lof/thumbnail_2.jpg" alt="" />
+                <img src="/images/lof/thumbnail_3.jpg" alt="" />
               </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
+
+              {/* collar */}
+              <div className="col-md-6 col-lg-4 collar">
+                <img src="/images/lof/collar_1.jpg" alt="" />
               </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
+              <div className="col-md-6 col-lg-4 collar">
+                <img src="/images/lof/collar_3.jpg" alt="" />
               </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
+              <div className="col-md-6 col-lg-4 collar">
+                <img src="/images/lof/collar_2.jpg" alt="" />
               </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
+
             </div>
           </div>
         </div>
@@ -371,39 +423,7 @@ class App extends Component {
               <div className="col-md-6 col-lg-4">
                 <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
               </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
-              <div className="col-md-6 col-lg-4">
-                <img src="http://www.ivang-design.com/svg-load/portfolio/photo-p.jpg" alt="" />
-              </div>
+              
             </div>
           </div>
         </div>
